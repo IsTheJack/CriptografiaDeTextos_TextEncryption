1d93b403d190021e8416edcf2a19672fb7c2e475
Roberto Oliveira da Silva <robertooliveiraestudo@gmail.com>
2014-06-17 01:06:41 -0300

    Project Upload

---
+++ EncriptadorDeMensagens/EncriptadorDeMensagens/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace EncriptadorDeMensagens
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtMensagem = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCriptografar = new System.Windows.Forms.Button();
+            this.btnDecodificar = new System.Windows.Forms.Button();
+            this.btnAjuda = new System.Windows.Forms.Button();
+            this.tipBalao = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(12, 28);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(414, 190);
+            this.txtMensagem.TabIndex = 0;
+            this.txtMensagem.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mensagem:";
+            // 
+            // btnCriptografar
+            // 
+            this.btnCriptografar.Location = new System.Drawing.Point(15, 224);
+            this.btnCriptografar.Name = "btnCriptografar";
+            this.btnCriptografar.Size = new System.Drawing.Size(75, 23);
+            this.btnCriptografar.TabIndex = 2;
+            this.btnCriptografar.Text = "Criptografar";
+            this.tipBalao.SetToolTip(this.btnCriptografar, "Torna a mensagem contida na caixa de texto uma mensagem criptografada.");
+            this.btnCriptografar.UseVisualStyleBackColor = true;
+            this.btnCriptografar.Click += new System.EventHandler(this.btnCriptografar_Click);
+            // 
+            // btnDecodificar
+            // 
+            this.btnDecodificar.Location = new System.Drawing.Point(110, 224);
+            this.btnDecodificar.Name = "btnDecodificar";
+            this.btnDecodificar.Size = new System.Drawing.Size(75, 23);
+            this.btnDecodificar.TabIndex = 3;
+            this.btnDecodificar.Text = "Decodificar";
+            this.tipBalao.SetToolTip(this.btnDecodificar, "Torna a mensagem criptografada contida na caixa de texto uma mensagem legível.");
+            this.btnDecodificar.UseVisualStyleBackColor = true;
+            this.btnDecodificar.Click += new System.EventHandler(this.btnDecodificar_Click);
+            // 
+            // btnAjuda
+            // 
+            this.btnAjuda.Location = new System.Drawing.Point(351, 224);
+            this.btnAjuda.Name = "btnAjuda";
+            this.btnAjuda.Size = new System.Drawing.Size(75, 23);
+            this.btnAjuda.TabIndex = 4;
+            this.btnAjuda.Text = "Ajuda";
+            this.btnAjuda.UseVisualStyleBackColor = true;
+            this.btnAjuda.Click += new System.EventHandler(this.btnAjuda_Click);
+            // 
+            // tipBalao
+            // 
+            this.tipBalao.AutomaticDelay = 1000;
+            this.tipBalao.IsBalloon = true;
+            this.tipBalao.Tag = "";
+            this.tipBalao.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.tipBalao.ToolTipTitle = "Criptografia";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 258);
+            this.Controls.Add(this.btnAjuda);
+            this.Controls.Add(this.btnDecodificar);
+            this.Controls.Add(this.btnCriptografar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMensagem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Criptografia de Mensagens";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMensagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCriptografar;
+        private System.Windows.Forms.Button btnDecodificar;
+        private System.Windows.Forms.Button btnAjuda;
+        private System.Windows.Forms.ToolTip tipBalao;
+    }
+}
+
---
+++ EncriptadorDeMensagens/EncriptadorDeMensagens/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EncriptadorDeMensagens
+{
+    public partial class frmPrincipal : Form
+    {
+        string sequencia = "";
+        int tamanho;
+        int[] arrayNumeroChar;
+
+        public frmPrincipal()
+        {
+            InitializeComponent(); //Construtor
+        }
+
+        public void Criptografar()
+        {
+            OperacaoDaCriptografia(true);
+        }
+
+        private void Decodificar()
+        {
+            OperacaoDaCriptografia(false);
+        }
+
+        //Se o valor do argumento do método abaixo for verdadeiro, ele criptografará,
+        //caso contrário, fará a ação inversa.
+        public void OperacaoDaCriptografia(bool criptografar) 
+        {
+            tamanho = txtMensagem.Text.Count();
+            arrayNumeroChar = new int[tamanho];
+            short sinal;
+
+            if (criptografar)
+            {
+                sinal = 1;
+            }
+            else
+            {
+                sinal = -1;
+            }
+      
+            for (int i = 0; i < tamanho; i++)
+            {
+                if (Convert.ToInt32(Convert.ToChar(txtMensagem.Text.Substring(i, 1))) != 13) //Se o caractere for diferenter de 'ENTER'
+                {
+                    if (i % 2 == 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0)
+                    {
+                        do
+                        {
+                            arrayNumeroChar[i] = Convert.ToInt32(Convert.ToChar(txtMensagem.Text.Substring(i, 1))) + 2 * sinal;
+                        } while (i % 3 == 0 || i % 5 == 0 || i % 7 == 0);
+                    }
+                    else if (i % 3 == 0 && i % 2 != 0 && i % 5 != 0 && i % 7 != 0)
+                    {
+                        do
+                        {
+                            arrayNumeroChar[i] = Convert.ToInt32(Convert.ToChar(txtMensagem.Text.Substring(i, 1))) + 3 * sinal;
+                        } while (i % 2 == 0 || i % 5 == 0 || i % 7 == 0);
+                    }
+                    else if (i % 5 == 0 && i % 2 != 0 && i % 3 != 0 && i % 7 != 0)
+                    {
+                        do
+                        {
+                            arrayNumeroChar[i] = Convert.ToInt32(Convert.ToChar(txtMensagem.Text.Substring(i, 1))) + 5 * sinal;
+                        } while (i % 2 == 0 || i % 3 == 0 || i % 7 == 0);
+                    }
+                    else if (i % 7 == 0 && i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
+                    {
+                        do
+                        {
+                            arrayNumeroChar[i] = Convert.ToInt32(Convert.ToChar(txtMensagem.Text.Substring(i, 1))) + 7 * sinal;
+                        } while (i % 2 == 0 || i % 3 == 0 || i % 5 == 0);
+                    }
+                    else
+                    {
+                        arrayNumeroChar[i] = Convert.ToInt32(Convert.ToChar(txtMensagem.Text.Substring(i, 1))) + 1 * sinal;
+                    }
+                }
+                else
+                {
+                    arrayNumeroChar[i] = 13; //'Enter' não é criptografado
+                }
+            }
+
+            TextoParaCaixa();
+        }
+
+        public void TextoParaCaixa()
+        {
+            for (int i = 0; i < tamanho; i++)
+            {
+                sequencia += Convert.ToString(Convert.ToChar(arrayNumeroChar[i]));
+            }
+            txtMensagem.Text = sequencia;
+            sequencia = "";
+        }
+
+        private void btnAjuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtMensagem.Text.Count().ToString());
+        }
+
+        private void btnCriptografar_Click(object sender, EventArgs e)
+        {
+            Criptografar();
+        }
+
+        private void btnDecodificar_Click(object sender, EventArgs e)
+        {
+            Decodificar();
+        }
+    }
+}
